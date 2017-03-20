cff8f2219dc942003d0ae6cb688030ab7fd30555
Madhu Hanlin <madhu.hanlin@corp.altegrity.com>
2017-03-20 16:24:42 -0500

    Deploying to Charlie - 03/20 @ 4.24pm

--- app/app.js
+++ app/app.js
@@ -270,10 +270,10 @@ angular
   })
  // this block below removes the hash tag from angular urls
 
-//   $locationProvider.html5Mode({
-//     enabled: true,
-//     requireBase: false
-//   });
+  $locationProvider.html5Mode({
+    enabled: true,
+    requireBase: false
+  });
 })
 
 .run(function ($rootScope, $location, $window) {
